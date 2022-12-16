f0843de5dec36b2125cf4db656df6f6e6eff3cd5
Marcus Lau <104477742+marcus-lau-slyp@users.noreply.github.com>
2022-12-16 14:23:22 +1100

    Feature/add tf test (#3)
    * ADD unit test
    
    * FIX syntax
    
    * FIX lambda
    
    * FIX lambda
    
    * FIX environment variables
    
    * FIX environment variables

--- tests/unit/main.tf
+++ tests/unit/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-module "lambda" {
+module "lambda_basic" {
   source = "../../terraform"
 
   bucket_lambda_code = aws_s3_bucket.temp.arn
@@ -10,6 +10,19 @@ module "lambda" {
   function_name	= "tf-mod-lambda-test"
   handler = "index.lambda_handler"
   runtime = "python3.9"
+}
+
+module "lambda_basic_vars" {
+  source = "../../terraform"
+
+  bucket_lambda_code = aws_s3_bucket.temp.arn
+  bucket_path_lambda_code	= aws_s3_object.temp.key
+  function_name	= "tf-mod-lambda-test"
+  handler = "index.lambda_handler"
+  runtime = "python3.9"
+  environment_variables = {
+    TEST = "test"
+  }
 }
 
 resource "aws_s3_bucket" "temp" {
