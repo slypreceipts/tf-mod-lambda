c22fa2a9272f05ea112d5dc76a9dc7a6246f7544
Marcus Lau <104477742+marcus-lau-slyp@users.noreply.github.com>
2022-12-20 00:42:35 +1100

    ADD output function name (#9)
    * ADD terraform provider striction
    
    * ADD terraform provider striction
    
    * UPDATE tf lock file
    
    * ADD function name output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "lambda_arn" {
   value = aws_lambda_function.lambda.arn
 }
 
+output "lambda_name" {
+  value = aws_lambda_function.lambda.function_name
+}
+
 output "lambda_alias_arn" {
   value = aws_lambda_alias.lambda.arn
 }
