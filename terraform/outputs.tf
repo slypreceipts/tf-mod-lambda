f9cd706188d28fa13615dc0f3c5535c32ee5ba2e
Marcus Lau <marcus.lau@slyp.com.au>
2022-12-19 12:06:44 +1100

    ADD alias name output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,10 @@ output "lambda_alias_arn" {
   value = aws_lambda_alias.lambda.arn
 }
 
+output "lambda_alias_name" {
+  value = aws_lambda_alias.lambda.name
+}
+
 output "lambda_alias_invoke_arn" {
   value = aws_lambda_alias.lambda.invoke_arn
 }
