24d3e33fc1a12ec09674c81f1220ca3349693935
Marcus Lau <104477742+marcus-lau-slyp@users.noreply.github.com>
2022-12-19 12:08:12 +1100

    Feature/add invoke arn (#6)
    * ADD invoke arn
    
    * UPDATE version
    
    * ADD alias name output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,14 @@ output "lambda_alias_arn" {
   value = aws_lambda_alias.lambda.arn
 }
 
+output "lambda_alias_name" {
+  value = aws_lambda_alias.lambda.name
+}
+
+output "lambda_alias_invoke_arn" {
+  value = aws_lambda_alias.lambda.invoke_arn
+}
+
 output "iam_role_arn" {
   value = aws_iam_role.lambda.arn
 }
