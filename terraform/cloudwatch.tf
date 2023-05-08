4992670aa9d568c60170c9c7ff5eb5bd6d587327
Marcus Lau <marcus.lau@slyp.com.au>
2023-05-08 13:12:38 +1000

    UPDATE add log retension

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,5 +1,7 @@
 resource "aws_cloudwatch_log_group" "lambda" {
   # checkov:skip=CKV_AWS_158:Ensure that CloudWatch Log Group is encrypted by KMS
+  # checkov:skip=CKV_AWS_338:Ensure CloudWatch log groups retains logs for at least 1 year
+
   name = "/aws/lambda/${var.function_name}${var.suffix}"
 
   retention_in_days = var.log_retention
